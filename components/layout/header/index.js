40d05a93a8a689dbce2c2cfba3ca94c7055497b7
“Ravi <kushwahravi7047@gmail.com>
2023-12-21 11:57:35 +0530

    21/dec

---
+++ components/layout/header/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import NavbarPage from "../navbarPage";
+
+export default function HeaderPage() {
+  return (
+    <header>
+      <NavbarPage />
+    </header>
+  );
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import LayoutPage from "@/components/layout";
 // import '@/styles/globals.css'
+import "@/styles/custom.css";
+import "@/styles/theme.min.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <LayoutPage>
+      <Component {...pageProps} />
+    </LayoutPage>
+  );
 }
